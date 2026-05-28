1f459b297b7d1d56c2b3d199e96eb3fbb353723a
maveliz25-byte <maveliz.25@est.ucab.edu.ve>
2026-05-27 23:13:39 -0400

    Update Publicacion.java
    del oldDated

--- backend/src/main/java/es/ucab/entrenos/modulos/publicacion/modelos/Publicacion.java
+++ backend/src/main/java/es/ucab/entrenos/modulos/publicacion/modelos/Publicacion.java
@@ -1,4 +1,34 @@
 package es.ucab.entrenos.modulos.publicacion.modelos;
 
 public class Publicacion {
+  private String idUsuario;
+    private String nombreUsuario;
+    private double reputacionUsuario;
+    private String tipoPublicacion; // "HABILIDAD" o "NECESIDAD"
+    private String nombreServicio;
+    private String descripcion;
+    private int precioCreditos;
+
+    public PublicacionDTO(String idUsuario, String nombreUsuario, double reputacionUsuario,
+                          String tipoPublicacion, String nombreServicio, String descripcion, int precioCreditos) {
+        this.idUsuario = idUsuario;
+        this.nombreUsuario = nombreUsuario;
+        this.reputacionUsuario = reputacionUsuario;
+        this.tipoPublicacion = tipoPublicacion;
+        this.nombreServicio = nombreServicio;
+        this.descripcion = descripcion;
+        this.precioCreditos = precioCreditos;
+    }
+
+    public PublicacionDTO() {
+    }
+
+    // Getters necesarios para la serialización de Jackson (JSON)
+    public String getIdUsuario() { return idUsuario; }
+    public String getNombreUsuario() { return nombreUsuario; }
+    public double getReputacionUsuario() { return reputacionUsuario; }
+    public String getTipoPublicacion() { return tipoPublicacion; }
+    public String getNombreServicio() { return nombreServicio; }
+    public String getDescripcion() { return descripcion; }
+    public int getPrecioCreditos() { return precioCreditos; }
 }
